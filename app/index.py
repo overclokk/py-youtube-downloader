5585d1b8b1754ac1d58b06550cf1f1ba90dad3fd
Redi Levendi <redilevendi.dev@gmail.com>
2022-04-08 21:41:58 +0200

    changed a bit of structure

--- app/index.py
+++ app/index.py
@@ -5,7 +5,6 @@ from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 import os
 
 # https://superuser.com/questions/1567253/how-to-download-chapters-of-a-youtube-video-as-separate-video-files
-link = "https://www.youtube.com/watch?v=2sPcpOhAK64"
 
 # https://medium.com/daily-python/python-script-to-download-youtube-videos-daily-python-6-c3788be5b6b1
 # try:
@@ -18,30 +17,36 @@ link = "https://www.youtube.com/watch?v=2sPcpOhAK64"
 #     exit()
 
 
-try:
-    #where to save
-    SAVE_PATH = "video"
+def main():
+    link = "https://www.youtube.com/watch?v=2sPcpOhAK64"
+    try:
+        #where to save
+        SAVE_PATH = "video"
 
-    yt = YouTube( link )
-    print( 'Title :', yt.title )
+        yt = YouTube( link )
+        print( 'Title :', yt.title )
+        # print('Available formats :')
+        # for stream in yt.streams.all():
+        # print(stream)
 
-#     print('Available formats :')
-#     for stream in yt.streams.all():
-#         print(stream)
+        # itag = input('\nEnter the itag number to download video of that format: ')
+        # stream = yt.streams.get_by_itag( itag )
+        stream = yt.streams.get_highest_resolution()
 
-#     itag = input('\nEnter the itag number to download video of that format: ')
-#     stream = yt.streams.get_by_itag( itag )
-    stream = yt.streams.get_highest_resolution()
+        print( '\nDownloading--- ' + yt.title + ' into location : ' + SAVE_PATH )
+        stream.download( SAVE_PATH )
 
-    print( '\nDownloading--- ' + yt.title + ' into location : ' + SAVE_PATH )
-    stream.download( SAVE_PATH )
+        print( stream.default_filename )
+        input_video_path = stream.default_filename
 
-    print( stream.default_filename )
-    input_video_path = stream.default_filename
+        ffmpeg_extract_subclip( SAVE_PATH + os.path.sep + input_video_path, 4380, 4699, targetname="clip.mp4")
 
-    ffmpeg_extract_subclip( SAVE_PATH + os.path.sep + input_video_path, 4380, 4699, targetname="clip.mp4")
+        input('Hit Enter to exit')
+    except Exception as e:
+        print("Error",e) #to handle exception
+        input('Hit Enter to exit')
 
-    input('Hit Enter to exit')
-except Exception as e:
-    print("Error",e) #to handle exception
-    input('Hit Enter to exit')
+
+
+if __name__ == "__main__":
+    main()
