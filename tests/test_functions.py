2f248105db90e06399d843abfc7985d03f0addeb
Enea <info@overclokk.net>
2022-04-16 17:33:00 +0200

    Refactored test

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -21,7 +21,7 @@ class FunctionsTestCase(unittest.TestCase):
             file_content,
             ''
         )
-        self.assertEqual(3,len(file_content),'')
+        self.assertEqual(3, len(file_content), '')
 
     def test_get_timestamps_from_file_with_chapters_format(self):
         file_content = parse_file('fixtures/chapters.txt')
@@ -34,10 +34,26 @@ class FunctionsTestCase(unittest.TestCase):
 
     def test_format_conversion(self):
         actual = [['00:15'], ['00:16'], ['00:17'], ['00:18'], ['00:19']]
-        expected = [['00:15','00:16'], ['00:16','00:17'], ['00:17','00:18'], ['00:18','00:19']]
+        expected = [['00:15', '00:16'], ['00:16', '00:17'], ['00:17', '00:18'], ['00:18', '00:19']]
 
         converted = convert_chapter_format_to_start_end_format(actual)
         self.assertEqual(expected, converted, '')
+
+    def test_video_finder(self):
+        content = [
+            '33 parte 2.txt', 'Era evitabile il conflitto_ Con V.E. Parsi.mp4',
+            'index.py',
+            'La Scuola Oltre Le Illusioni CONVEGNO sabato 9 aprile.mp4',
+            "Storia dell'Ucraina conoscere il contesto per capire la guerra.txt"
+        ]
+
+        video_list = []
+        for file in content:
+            if file.find('.mp4') < 1:
+                continue
+            video_list.append( file )
+
+        print(video_list)
 
 
 if __name__ == '__main__':
